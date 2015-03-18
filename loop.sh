86d3219e444884c0629699d850d663dc7e218487
hedi <hedi02@gmail.com>
2015-03-18 19:05:38 +0100

    commit March, 18

--- loop.sh
+++ loop.sh
@@ -1,10 +1,13 @@
 rm -f cwnd
 #$random=1 + $[ RANDOM % 10 ]
 #mv -f cwnd bck/cwnd
-for round in `seq 1 $1`;
+for rts in 50 5000
 do
- for iteration in `seq 1 10`;
+ for node in `seq $1 $2`;
  do
-  ../waf --run "wifi-simple-infra2 --n=$round" >> cwnd  
+  for iteration in `seq 1`;
+  do
+   ../waf --run "wifi-simple-infra2 --n=$node --rtsCts=$rts" >> cwnd  
+  done
  done
 done
--- plot.plt
+++ plot.plt
@@ -7,4 +7,4 @@ set ylabel "Throughput (Mbps)"
 set key outside center below
 set datafile missing "-nan"
 set style line 1 lc rgb '#0060ad' lt 1 lw 2 pt 7 ps 1.5   # --- blue
-plot 'cwnd' with linespoints, "cwnd" index 1 with linespoints
+plot 'cwnd' with linepoints, "cwnd" index 1 with linepoints
